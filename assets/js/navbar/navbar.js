bea6546aa8c2850c6eb017b733339033a8d34784
DamienFoulon <foulon.damien.pro@gmail.com>
2022-04-21 22:08:55 +0000

    index page on Desktop

--- assets/js/navbar/navbar.js
+++ assets/js/navbar/navbar.js
@@ -1,8 +1,7 @@
 const navbarToggle = document.querySelector('.navbarToggle');
 const navRight = document.querySelector('.nav-right');
 const home = document.querySelector('.home');
-
-navbarToggle.addEventListener("click", openNav);
+const navItems = document.querySelectorAll('.navbar a');
 
 function openNav() {
   if (navRight.style.visibility === "hidden" && window.screen.width <= 1200) {
@@ -13,4 +12,21 @@ function openNav() {
     navRight.style.visibility = "hidden";
     home.style.visibility = "hidden";
   }
-} 
+}
+
+function setActive(){
+  let current = document.querySelector('.active');
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+    current.classList.remove('active');
+  } else {
+    this.classList.add('active');
+    current.classList.remove('active');
+  }
+}
+
+for(let i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener('click', setActive);
+}
+
+navbarToggle.addEventListener("click", openNav);
